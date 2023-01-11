6ed80e3f7f77e880c1b917b901b603f5051319db
Shaheryar Abid <sharyar0310@gmail.com>
2023-01-11 12:58:03 +0500

    Added script.js file

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
-// checking if local storage is empty than add an empty array
-if (localStorage.getItem('Added Books') === null) {
-  localStorage.setItem('Added Books', JSON.stringify([]));
-}
-// store data into local storage
-const storeData = JSON.parse(localStorage.getItem('Added Books'));
+/* eslint-disable*/
+const data = document.querySelector('#data');
 
-function updateData() {
-  localStorage.setItem('Added Books', JSON.stringify(storeData));
-}
-
-function createBooks(arr) {
-  let books = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    books += `
-            <p>${arr[i].title}</p>
-            <p>${arr[i].author}</p>
-            <button onclick="removeBook(${i})">Remove</button>
-            <hr/>
-            `;
+// Book class: Represent a book
+class Book {
+  constructor(title, authore) {
+    this.title = title;
+    this.authore = authore;
   }
-  return books;
-}
-// Diplaying data to the UI from local storage
-function displayBooks() {
-  const listOfBooks = document.querySelector('.container');
-  listOfBooks.innerHTML = `
-              <ul class="book-ul"/>
-              ${createBooks(storeData)}</ul>
-          `;
 }
 
-// Adding new data in the local storage
-function addNewdata(bookTitle, bookAuthor) {
-  const Book = {
-    title: bookTitle,
-    author: bookAuthor,
-  };
-  storeData.push(Book);
-  updateData();
-  displayBooks();
-}
-// Remove data from local storage
-function removeBook(i) {
-  storeData.splice(i, 1);
-  updateData();
-  displayBooks();
-}
-removeBook();
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const books = store.getBooks();
 
-// Getting values from input fields
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  const title = document.querySelector('.title');
-  const author = document.querySelector('.author');
-  e.preventDefault();
-  addNewdata(title.value, author.value);
-});
+    books.forEach((book) => UI.addBookToList(book));
+  }
 
-window.onload = displayBooks();
+  static addBookToList(book) {
+    const list = document.querySelector('#data');
+
+    const div = document.createElement('div');
+    div.className = 'data-container';
+
+    div.innerHTML = `
+        <p>"${book.title}"</p>
+        <p>by</p>
+        <p>${book.authore}</p>
+        <div><button id="remove-btn" class='delete'>Remove</button></div>
+     `;
+
+    list.appendChild(div);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearField() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+}
+
+// store data in local storage
+class store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(authore) {
+    const books = store.getBooks();
+    books.forEach((book, index) => {
+      if (book.authore === authore) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
