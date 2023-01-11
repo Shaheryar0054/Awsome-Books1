f55ce1a83057ce2028f3833e370abb46bfe07cc2
Lancelot-SO <fsowah001@gmail.com>
2023-01-11 08:12:35 +0000

    modified script.js file

--- script.js
+++ script.js
@@ -76,3 +76,34 @@ class store {
     localStorage.setItem('books', JSON.stringify(books));
   }
 }
+
+//  Event for display books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Add event listener to the add button
+document.querySelector('#form').addEventListener('submit', (e) => {
+// Prevent actual submit
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const authore = document.querySelector('#author').value;
+
+  // Instatiate book
+  const book = new Book(title, authore);
+
+  // Add book data to screen
+  UI.addBookToList(book);
+
+  // Add book to store
+  store.addBook(book);
+
+  // clear fields
+  UI.clearField();
+});
+
+// Event to remove books
+document.querySelector('#data').addEventListener('click', (e) => {
+// Remove book from UI
+  UI.deleteBook(e.target);
+  // Remove book from local storage
+  store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+});
